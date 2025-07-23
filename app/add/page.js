a16a24341b407288f24fcd5b60e9c44508e85a2d
Tomislav Dukez <tomdu3@ymail.com>
2025-07-23 16:05:29 +0100

    add add recipe page

---
+++ app/add/page.js
@@ -0,0 +1,10 @@
+export default function Add() {
+    return (
+        <div>
+            <form className="text-center font-bold p-20">
+                Add recipe
+            </form>
+        </div>
+    )
+
+}
